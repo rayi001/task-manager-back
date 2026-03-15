27e2bfa3a7fe28dc3992b020b04ce16f2dda0bc1
rayi001 <rayi001@ess.ais.ac.nz>
2026-03-15 18:29:55 +1300

    3-environment-variables-configuration

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*92pe!qh02g2vjro!j$2*)%@$&t@7o)j&rr11c3e-#v4__&1yz'
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-*92pe!qh02g2vjro!j$2*)%@$&t@7o)j&rr11c3e-#v4__&1yz')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
 
 
 # Application definition

3ade1f960cad972e03c076502922f2569e7a6016
rayi001 <rayi001@ess.ais.ac.nz>
2026-03-15 18:47:57 +1300

    4-user-authentication

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,5 +1,30 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models import Task
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+    password_confirm = serializers.CharField(write_only=True)
+    
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'password_confirm')
+    
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password_confirm']:
+            raise serializers.ValidationError("Passwords don't match")
+        return attrs
+    
+    def create(self, validated_data):
+        validated_data.pop('password_confirm')
+        user = User.objects.create_user(**validated_data)
+        return user
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'date_joined')
+        read_only_fields = ('id', 'date_joined')
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:

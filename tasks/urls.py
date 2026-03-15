2f9dc34ee8c3669ccfaa81227c9dc7b638812497
rayi001 <rayi001@ess.ais.ac.nz>
2026-03-15 19:29:43 +1300

    6-task-status-management

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,15 +1,9 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenRefreshView
 from . import views
 
 urlpatterns = [
-    # Authentication URLs
-    path('register/', views.register, name='register'),
-    path('login/', views.login, name='login'),
-    path('profile/', views.profile, name='profile'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    
     # Task URLs
     path('tasks/', views.TaskListCreateView.as_view(), name='task-list-create'),
     path('tasks/<int:pk>/', views.TaskDetailView.as_view(), name='task-detail'),
+    path('tasks/<int:pk>/status/', views.update_task_status, name='update-task-status'),
 ]

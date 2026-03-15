2a2664cb77a985bcc04c122cc8141eafd0518fb6
rayi001 <rayi001@ess.ais.ac.nz>
2026-03-16 10:46:22 +1300

    Fixes permission issues

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenRefreshView
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from authentication import views as auth_views
 
 @api_view(['GET'])
+@permission_classes([AllowAny])
 def api_root(request):
     return Response({
         'message': 'Task Manager API',

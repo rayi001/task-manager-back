a67b09be61a65248f51da282448eea1d38621c46
rayi001 <rayi001@ess.ais.ac.nz>
2026-03-16 19:57:35 +1300

    Add auto-migration to WSGI for serverless environments

--- task_manager/wsgi.py
+++ task_manager/wsgi.py
@@ -9,8 +9,16 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
+from django.core.management import execute_from_command_line
 
 # Set Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_manager.settings')
+
+# Run migrations on startup for serverless environments
+if os.environ.get('VERCEL') or os.environ.get('AWS_LAMBDA_FUNCTION_NAME'):
+    try:
+        execute_from_command_line(['manage.py', 'migrate', '--noinput'])
+    except Exception as e:
+        print(f"Migration error: {e}")
 
 application = get_wsgi_application()

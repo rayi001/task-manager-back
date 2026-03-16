1373b5178754faabd80baa512a9e6bb1ddd74e77
rayi001 <rayi001@ess.ais.ac.nz>
2026-03-16 16:37:22 +1300

    backend permission issue fixes

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+# Build React frontend
+cd frontend && npm install && npm run build && cd ..
+
+# Build Django backend
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
